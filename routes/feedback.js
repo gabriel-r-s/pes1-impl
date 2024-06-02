bbffbf77995c8a17f4692756f0930e5283088b67
gabriel-r-s <author@email>
2024-06-02 11:09:57 -0300

    add sqlite3

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,72 +1,80 @@
+const sqlite3 = require('sqlite3').verbose();
+
 class Feedback {
-    constructor(id, text, lido) {
+    constructor(id, txt, read) {
         this.id = id;
-        this.text = text;
+        this.txt = txt;
         this.read = false;
-        this.deleted = false;
     }
 }
 
 class FeedbackManager {
     constructor() {
-        this.feedbacks = [
-            new Feedback(0, "Deleniti nihil autem totam qui. Ipsam quod animi sunt perspiciatis. Accusantium perferendis vel dolorem dolore nostrum ut aperiam. Dolores facilis adipisci sed sint praesentium aut aperiam natus. Repudiandae unde perferendis saepe et voluptatibus assumenda. Dolor aut expedita dolore.", false, false),
-            new Feedback(1, "Molestiae aut nihil minima quis sit mollitia reprehenderit nihil. Dolorem aut eos qui ipsum earum qui voluptatum laudantium. Ex minus unde consequatur esse voluptas aliquid esse. Sed deleniti tempore ratione. Voluptatem excepturi voluptas ipsam rerum dicta aut.", false, false),
-            new Feedback(2, "Illo consequatur animi ut cum porro qui ullam doloribus. Praesentium suscipit sint eum. Sint consequuntur neque voluptatum. Ut consequatur non laudantium porro dolorem repellat. Est numquam eos repellendus saepe inventore est aliquid quae. Suscipit qui molestias repellat quo quod.", false, false),
-            new Feedback(3, "Praesentium nostrum et molestiae neque error repellat. Atque voluptatem sint unde rerum veritatis nobis illum. Voluptatem quia praesentium minima eius asperiores non non ut.", false, false),
-            new Feedback(4, "Voluptas rerum et rerum. Consequatur nobis molestiae sapiente accusamus. Facilis voluptates dicta sed debitis eveniet ut. Laboriosam esse dolorem et aliquid molestias est deleniti nisi. Error fugit culpa nihil at atque provident enim aut.", false, false)
+        this.db = new sqlite3.Database("./db.sqlite3");
+        const lorem = [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida metus id augue volutpat varius. Quisque posuere ligula et tellus pellentesque, ac finibus enim convallis.",
+            "Quisque imperdiet pretium mauris, in facilisis leo pretium quis. Aenean ut nisi justo. Nunc odio neque, posuere nec viverra a, fringilla sit amet neque. Morbi id massa ultrices, pharetra lorem nec, rutrum turpis. Nullam pharetra tincidunt luctus. Suspendisse ac magna eleifend, viverra arcu eu, consequat augue. Phasellus elementum nisl nisi.",
+            "Duis iaculis varius erat scelerisque mattis. Duis vel tellus et urna egestas posuere. Fusce nec quam mauris. Nulla faucibus purus sed fringilla posuere.",
+            "Integer finibus elementum arcu eu elementum. Proin eleifend ligula mi, vitae auctor odio interdum in. Pellentesque a diam ex. Fusce interdum metus et risus fringilla, at finibus mauris commodo. Nam in felis arcu. Maecenas accumsan vulputate tincidunt."
         ];
+        for (let i = 0; i < lorem.length; i++) {
+            this.db.run("INSERT OR REPLACE INTO FEEDBACKS(id, txt, read) VALUES (?, ?, ?)", i+1, lorem[i], false);
+        }
     }
 
     get(req, res) {
         const { id } = req.query;
         if (id !== undefined) {
-            if (id >= this.feedbacks.length || this.feedbacks[id].deleted) {
-                return res.status(400);
-            }
             console.log(`GET ${id}`);
-            res.json(this.feedbacks[id]);
-            return res.status(200);
+            this.db.get("SELECT * FROM feedbacks WHERE id=(?)", id, (err, row) => {
+                if (row === undefined) {
+                    return res.status(400);
+                } else {
+                    res.json(row);
+                    return res.status(200);
+                }
+            });
         } else {
-            const non_deleted = this.feedbacks.filter((f) => { return !f.deleted });
-            res.json(non_deleted);
-            for (let feedback of this.feedbacks) {
-                feedback.read = true;
-            }
             console.log(`GET`);
+            this.db
+                .all("SELECT * FROM feedbacks", (err, rows) => {
+                res.json(rows);
+                return res.status(200);
+            })
+                .run("UPDATE feedbacks SET read=true");
             return res.status(200);
         }
     }
 
     post(req, res) {
-        const { text } = req.query;
-        console.log(`POST {text}`);
-        const id = this.feedbacks.length;
-        const feedback = new Feedback(id, text, false, false);
-        this.feedbacks.push(feedback);
+        const { txt } = req.query;
+        console.log(`POST ${txt}`);
+        this.db.run("INSERT INTO feedbacks(txt, read) VALUES(?, false)", txt);
         return res.status(200);
     }
 
     put(req, res) {
-        const { id, text } = req.query;
-        console.log(`PUT ${id} ${text}`)
-        if (id === undefined || id >= this.feedbacks.length || this.feedbacks[id].deleted) {
+        const { id, txt } = req.query;
+        console.log(`PUT ${id} ${txt}`)
+        if (id === undefined || txt === undefined) {
             return res.status(400);
         }
-        if (text !== undefined) {
-            this.feedbacks[id].text = text;
-        }
-        return res.status(200);
+        this.db.get(
+            "UPDATE feedbacks SET txt=(?) WHERE id=(?) RETURNING (1)", txt, id,
+            (err, row) => { return res.status(row === undefined ? 400 : 200); }
+        );
     }
 
     del(req, res) {
         const { id } = req.query;
         console.log(`DELETE ${id}`);
-        if (id === undefined || id >= this.feedbacks.length || this.feedbacks[id].deleted) {
-            return res.status(400);
-        }
-        this.feedbacks[id].deleted = true;
-        return res.status(200);
+        this.db.get("DELETE FROM feedbacks WHERE id=(?) RETURNING (1)", id, (err, row) => {
+            if (row === undefined) {
+                return res.status(400);
+            } else {
+                return res.status(200);
+            }
+        })
     }
 }
 
